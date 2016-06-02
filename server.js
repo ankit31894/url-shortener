77a7bb1e62ca4dbd113d035b3cd6947018094bf7
Ankit <ankit31894@gmail.com>
2016-06-03 02:57:35 +0530

    initial files

---
+++ server.js
@@ -0,0 +1,103 @@
+//lets require/import the mongodb native drivers.
+var mongodb = require('mongodb');
+var express = require("express");
+var http = require("http");
+var checkUrl=require("./validUrl");
+var app = express();
+
+//We need to work with "MongoClient" interface in order to connect to a mongodb server.
+var MongoClient = mongodb.MongoClient;
+
+// Connection URL. This is where your mongodb server is running.
+
+//(Focus on This Variable)
+
+var json_res={};
+app.get("/", function(request, response) {
+    response.writeHead(200, { "Content-Type": "text/plain" });
+    response.write("Use /new/url to get the tinyurl of a site\n");
+    response.end("Or enter  the tinyurl of a site");
+});
+app.get("/new/*", function(request, response,next) {
+      response.writeHead(200, { "Content-Type": "text/plain" });
+
+    var url=request.params[0];
+    if(checkUrl(url)){
+        json_res={
+            "url":url,
+        }
+
+        var mongo_url = 'mongodb://localhost:27017/my_database_name';      
+        //(Focus on This Variable)
+
+        // Use connect method to connect to the Server
+        MongoClient.connect(mongo_url, function (err, db) {
+            if (err) {
+                console.log('Unable to connect to the mongoDB server. Error:', err);
+            }
+            else {
+                console.log('Connection established to', mongo_url);
+                var collection = db.collection('urls')
+                collection.find().limit(1).sort({$natural:-1}).toArray(function(err,doc){
+                    if(doc.length===0)
+                        json_res.id=0;
+                    else 
+                        json_res.id=doc[0].id+1;
+                     collection.insert(json_res, function(err,s) {
+                         json_res.tiny_url=(request.protocol + '://' + request.get('host') + request.url).replace("new/"+request.params[0],"")+json_res.id;
+                        var str=JSON.stringify(json_res);
+                        response.end(str);
+                        db.close();
+                     });
+                });
+            }
+        });
+    }
+    else{
+         json_res={
+            "url":"wrong url"
+         }
+        var str=JSON.stringify(json_res);
+        response.end(str);
+
+    }
+    
+});
+app.get("/:number", function(request, response,next) {
+
+    
+    var mongo_url = 'mongodb://localhost:27017/my_database_name';      
+    //(Focus on This Variable)
+
+    // Use connect method to connect to the Server
+    MongoClient.connect(mongo_url, function (err, db) {
+        if (err) {
+            console.log('Unable to connect to the mongoDB server. Error:', err);
+        }
+        else {
+            console.log('Connection established to', mongo_url);
+            var collection = db.collection('urls')
+            collection.find({"id":parseInt(request.params.number)}).limit(1).toArray(function(err,doc){
+                if(doc.length===0){
+                    json_res={"result":"no url to redirect"};
+                    response.end(JSON.stringify(json_res));
+                }
+                else {
+                    response.setHeader("Location", doc[0].url);
+                    response.writeHead(302);
+                    response.end();
+                }
+                db.close();
+            });
+        }
+    });    
+    
+});
+app.get("*", function(request, response) {
+    response.end("404!");
+});
+http.createServer(app);
+var port = process.env.PORT || 8080;
+app.listen(port,  function () {
+	console.log('Node.js listening on port ' + port + '...');
+});
---
+++ validUrl.js
@@ -0,0 +1,11 @@
+//reference http://stackoverflow.com/a/14582229/1533609
+
+module.exports=function(str) {
+  var pattern = new RegExp('^(https?:\\/\\/)'+ // protocol
+  '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.?)+[a-z]{2,}|'+ // domain name
+  '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
+  '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
+  '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
+  '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
+  return pattern.test(str);
+}
